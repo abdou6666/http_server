447ffa25a50a38973b9f37f09ac6a1fed0e77b56
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2024-05-19 21:31:41 +0100

    get /

--- app/main.js
+++ app/main.js
@@ -1,7 +1,14 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    socket.on('data', (data) => {
+        const data = data.toString();
+        if (data.startWith('GET /')) {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+    });
 
     socket.on("close", () => {
         socket.end();
