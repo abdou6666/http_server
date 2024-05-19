9e22f4459102371cee44b28f25ef059e42d74f43
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2024-05-19 21:37:31 +0100

    last fix

--- app/main.js
+++ app/main.js
@@ -3,14 +3,12 @@ const net = require("net");
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
         const request = data.toString();
-        const splited = request.split('\n');
-        console.log(splited);
-        console.log({ request });
         if (request.startsWith('GET / ')) {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
+        socket.end();
     });
 
     socket.on("close", () => {
