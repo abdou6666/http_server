226060dd4d083c708cadbc074f52bc2359a12b40
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2024-05-19 21:22:20 +0100

    finished

--- app/main.js
+++ app/main.js
@@ -1,6 +1,8 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
     socket.on("close", () => {
         socket.end();
         server.close();
