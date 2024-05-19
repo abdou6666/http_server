7af02ccd7c35a64c9ee0c689637ad776e213f3fd
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2024-05-19 21:32:41 +0100

    fix

--- app/main.js
+++ app/main.js
@@ -2,8 +2,8 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
-        const data = data.toString();
-        if (data.startWith('GET /')) {
+        const request = data.toString();
+        if (request.startsWith('GET /')) {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
         } else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
