0c13798755f5e6504eba6d7af438136fcf6db483
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2024-05-30 16:58:05 +0100

    concurrent

--- app/main.js
+++ app/main.js
@@ -2,44 +2,25 @@ const net = require("net");
 
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
+        console.log('Received data:\n', data.toString());
+
         const request = data.toString();
-        if (request.startsWith('GET / ')) {
-            socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        } else if (request.includes("/user-agent")) {
+        const [requestLine] = request.split('\r\n');
+        const [method, url] = requestLine.split(' ');
 
-            const splited = request.split('\n\r');
-            console.log(request);
-            let index = splited[0].indexOf('User-Agent:');
-            let agent = '';
-            for (let i = index; i < i < splited[0].length; i++) {
-                if (splited[0][i] === '\r') {
-                    break;
-                }
-                agent += splited[0][i];
+        console.log(`Method: ${method}, URL: ${url}`);
 
-            }
-            const agentContent = agent.substring(12, agent.length);
-            const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${agentContent.length}\r\n\r\n${agentContent}`;
+        const response = "HTTP/1.1 200 OK\r\n\r\n";
 
+        socket.write(response);
 
-            socket.write(response);
-        } else if (request.includes("/echo/")) {
-            const content = request.split("/echo/")[1];
-            let endpoint = '';
-            for (let i = 0; i < content.length; i++) {
-                if (content[i] === ' ') {
-                    break;
-                }
-                endpoint += content[i];
-            }
-            const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${endpoint.length}\r\n\r\n${endpoint}`;
-            socket.write(response);
-        }
-        else {
-            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
-        }
         socket.end();
     });
+
+    socket.on('error', (err) => {
+        console.error('Socket error:', err);
+    });
+
 
     socket.on("close", () => {
         socket.end();
