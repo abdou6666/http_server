ba4a7c7dea59ed433369f5a79e85294f6eaee8f1
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2024-05-30 16:39:49 +0100

    User-agent

--- app/main.js
+++ app/main.js
@@ -5,11 +5,24 @@ const server = net.createServer((socket) => {
         const request = data.toString();
         if (request.startsWith('GET / ')) {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        }
-        else if (request.includes("/echo/")) {
-            const content = request.split("/echo/")[1];
+        } else if (request.includes("/user-agent")) {
 
-            // console.log(content.length);
+            const splited = request.split('\n\r');
+
+            let index = splited[0].indexOf('User-Agent:');
+            let agent = '';
+            for (let i = index; i < i < splited[0].length; i++) {
+                agent += splited[0][i];
+                if (splited[0][i] === '\r') {
+                    break;
+                }
+            }
+            const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${agent.length}\r\n\r\n${agent}`;
+
+
+            socket.write(response);
+        } else if (request.includes("/echo/")) {
+            const content = request.split("/echo/")[1];
             let endpoint = '';
             for (let i = 0; i < content.length; i++) {
                 if (content[i] === ' ') {
@@ -18,7 +31,6 @@ const server = net.createServer((socket) => {
                 endpoint += content[i];
             }
             const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${endpoint.length}\r\n\r\n${endpoint}`;
-            console.log({ response });
             socket.write(response);
         }
         else {
