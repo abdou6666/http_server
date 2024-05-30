702e9c85724433087f1913e2305e2c1515d23393
abdou6666 <sfayhi.abderrahmen@esprit.tn>
2024-05-30 12:55:01 +0100

    dynamic-endpoint

--- app/main.js
+++ app/main.js
@@ -5,7 +5,12 @@ const server = net.createServer((socket) => {
         const request = data.toString();
         if (request.startsWith('GET / ')) {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        } else {
+        }
+        else if (request.includes("/echo/")) {
+            const content = request.split("/echo/")[1];
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
+        }
+        else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
         socket.end();
